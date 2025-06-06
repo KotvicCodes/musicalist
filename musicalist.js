f46edb022dc73c602ac3a75530e9efb13935a0fe
Kotvič <134196364+KotvicCodes@users.noreply.github.com>
2025-06-06 11:52:39 +0200

    added basic script structure
    1.0
    added the whole script structure excluding writing down the genres

--- musicalist.js
+++ musicalist.js
@@ -2,10 +2,33 @@
 
 //! Import
 
-const puppeteer = require('puppeteer')
+import puppeteer from 'puppeteer'
 
 
-//! Puppeteering
+//! Input Data
 
-async function run(movie) {
+const song = "Shape of You"
+
+
+//! Puppeteering Genres
+
+async function genres(song) {
+     //* Open the website
+     const browser = await puppeteer.launch({ headless: false })
+     const page = await browser.newPage()
+     await page.goto(`https://www.chosic.com/music-genre-finder/`)
+
+     //* Search for your song
+     await page.waitForSelector("#search-word")
+     await page.type("#search-word", song, { delay: (Math.random() + 1) * 50 })
+     await page.waitForSelector(".span-class")
+     await page.click(".span-class")
+
+     //* Scrape genres
+     await page.waitForNavigation()
+     await page.screenshot({ path: 'musicalist.png', fullPage: true })
+     
+     //* Close down
+     await browser.close()
 }
+genres(song)

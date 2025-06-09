1be27201a4f768b0a292520e744cd87731f3a650
Kotvič <kotviclp@gmail.com>
2025-06-09 08:49:35 +0200

    added anti-cookie extension
    1.0
    Added an extension which automatically allows all cookies, as they are cleared from session to session anyways. It's called "I still don't care about cookies" v1.1.4, thanks to the authors, you're amazing <3 (https://github.com/OhMyGuus/I-Still-Dont-Care-About-Cookies?tab=GPL-3.0-1-ov-file#readme)
    1.1
    automatically added the extension to the Puppeteering browser upon starting
    1.2
    commented out further script for now

--- musicalist.js
+++ musicalist.js
@@ -3,6 +3,7 @@
 //! Import
 
 import puppeteer from 'puppeteer'
+import path from 'path'
 
 
 //! Input Data
@@ -13,20 +14,29 @@ const song = "Shape of You"
 //! Puppeteering Genres
 
 async function genres(song) {
-     //* Open the website
-     const browser = await puppeteer.launch({ headless: false })
+     //* Start the browser with extension
+     const extensionPath = path.resolve("no-cookies")
+     const browser = await puppeteer.launch({ 
+          headless: false,
+          args: [`--load-extension=${extensionPath}`],
+          ignoreDefaultArgs: ["--disable-extensions"]
+     })
      const page = await browser.newPage()
+
+     //* Open the website
      await page.goto(`https://www.chosic.com/music-genre-finder/`)
+     await new Promise(resolve => setTimeout(resolve, 10000))
+     await page.screenshot({ path: 'musicalist.png', fullPage: true })
 
      //* Search for your song
-     await page.waitForSelector("#search-word")
+/*   await page.waitForSelector("#search-word")
      await page.type("#search-word", song, { delay: (Math.random() + 1) * 50 })
      await page.waitForSelector(".span-class")
      await page.click(".span-class")
 
      //* Scrape genres
      await page.waitForNavigation()
-     await page.screenshot({ path: 'musicalist.png', fullPage: true })
+     await page.screenshot({ path: 'musicalist.png', fullPage: true }) */
      
      //* Close down
      await browser.close()
